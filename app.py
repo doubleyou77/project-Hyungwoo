3ce608d5bbf58b491df8b76ec37c076992bd7a3a
doubleyou77 <86065428+doubleyou77@users.noreply.github.com>
2023-07-13 14:29:40 +0900

    check

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
+
+from werkzeug.serving import WSGIRequestHandler
 
 app = Flask(__name__)
 
+# @app.route('/v1', method = ['POST']) # 경로 api
+# def convert_doc():
+#     data = request.get_json()
+
+#     if 'image' not in data:
+#         return "", 400
+    
+#     doc_img_data = 1
+#     #doc_img_data = () # 이미지 스캔화
+
+#     return doc_img_data, 200
+
 @app.route("/")
 def index():
-    return render_template('./index.html')
+    return "<h1>check page</h1>"
+
+if __name__ == "__main__":
+    # https://stackoverflow.com/questions/63765727/unhandled-exception-connection-closed-while-receiving-data
+    WSGIRequestHandler.protocol_version = "HTTP/1.1"
+    app.run(threaded=True, host='0.0.0.0', port=5000)
